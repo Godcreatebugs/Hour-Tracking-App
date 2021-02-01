3d5da0c06396e84231201b739bfc2588f45b4d66
Godcreatebugs <bhuvapiyush63@gmail.com>
2021-02-01 08:56:07 -0500

    app working with windows platform problem

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,35 @@
-<template lan="ht">
-  <div id="app">
-    <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-button v-b-toggle.sidebar>Menu</b-button>
-    <b-navbar-brand class="ml-4" href="#">NavBar</b-navbar-brand>
+
+<template>
+<div id="app">
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-button v-b-toggle.sidebar class="mr-4">Menu</b-button>
+    <b-navbar-brand href="#">Hours Tracking</b-navbar-brand>
   </b-navbar>
+  <b-sidebar id="sidebar" title="Main Menu" shadow>
+    <div class="px-3 py-2">
+      <b-list-group>
+        <b-list-group-item href="/#/">
+          Hours Log
+        </b-list-group-item>
+        <b-list-group-item href="/#/projects">
+          Projects
+        </b-list-group-item>
+      </b-list-group>
+      <div class="dropdown-divider"></div>
+      <b-list-group>
+        <b-list-group-item href="/">
+          Exports
+        </b-list-group-item>
+        <b-list-group-item href="/#/settings">
+          Settings
+        </b-list-group-item>
+      </b-list-group>
+    </div>
+  </b-sidebar>
 
-    <b-sidebar id="sidebar" title="Main Menu" shadow>
-      <div class="px-3 py-2">
-        <b-list-group>
-           <b-list-group-item href="/#/">
-            Hours
-          </b-list-group-item>
-          <b-list-group-item href="/#/projects">
-              Projects
-           </b-list-group-item>
-           ​<div class="dropdown-divider"></div>
-           <b-list-group-item >
-             Exports
-           </b-list-group-item>
-           <b-list-group-item >
-             Settings
-           </b-list-group-item>
-        </b-list-group>
-      </div>
-    </b-sidebar>
-    <router-view />
-  </div>
+  <router-view />
+</div>
 </template>
-
 
 <style>
 #app {
--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,36 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import HoursLog from '@/views/HoursLog.vue';
-import Projects from '@/views/Projects.vue';
+import HoursLog from '../views/HoursLog.vue';
+import TimeEntry from '../views/TimeEntry.vue';
+import Projects from '../views/Projects.vue';
 import ProjectEntry from '../views/ProjectEntry.vue';
 import Settings from '../views/Settings.vue';
 import SettingsEdit from '../views/SettingsEdit.vue';
+
 Vue.use(VueRouter);
 
-// defining routes
-// eslint-disable-next-line no-unused-vars
 const routes = [
   {
-    path : '/',
-    name : 'HoursLog',
-    component : HoursLog
+    path: '/',
+    name: 'HoursLog',
+    component: HoursLog
   },
   {
-    path:'/projects',
+    path: '/add-time-entry',
+    name: 'TimeEntry',
+    component: TimeEntry
+  },
+  {
+    path: '/projects',
     name: 'Projects',
-    comeponent :Projects
+    component: Projects
   },
   {
-   path: '/add-project',
-   name: 'ProjectEntry',
-   component: ProjectEntry
- },
- {
+    path: '/add-project',
+    name: 'ProjectEntry',
+    component: ProjectEntry
+  },
+  {
     path: '/settings',
     name: 'Settings',
     component: Settings
